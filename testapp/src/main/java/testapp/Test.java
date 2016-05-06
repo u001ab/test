e41c2a0f3d4bf7598d7f0119cf81dc2fc09c7211
u001ab <aaa_11aaa_11@126.com>
2016-05-06 15:25:21 +0800

    Update Test.java

--- testapp/src/main/java/testapp/Test.java
+++ testapp/src/main/java/testapp/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("aaa");
-		System.out.println("aaa1bbb");
+		System.out.println("aaa1dddddddbbb");
 		System.out.println("aaa1bbb");
 		
 
