5426bb835146c8f2f17aa948ce310f49bd79c576
li <li@aa.aa>
2016-05-06 15:36:00 +0800

    aaaaaaaaaaa

--- testapp/src/main/java/testapp/Test.java
+++ testapp/src/main/java/testapp/Test.java
@@ -4,11 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("aaa");
-
 		System.out.println("aaa1bbbddddd");
-
 		System.out.println("aaa1dddddddbbb");
-
 		System.out.println("aaa1bbb");
 		
 
